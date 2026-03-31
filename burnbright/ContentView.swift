aeee4d403507f7e3282df60f49e1497a4be4857d
Aksharaa Ramesh <aksharaaramesh.computing@gmail.com>
2026-03-31 18:41:56 +0800

    Initial Commit

---
+++ burnbright/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  burnbright
+//
+//  Created by Aksharaa Ramesh on 31/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ burnbright/burnbrightApp.swift
@@ -0,0 +1,17 @@
+//
+//  burnbrightApp.swift
+//  burnbright
+//
+//  Created by Aksharaa Ramesh on 31/3/26.
+//
+
+import SwiftUI
+
+@main
+struct burnbrightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
