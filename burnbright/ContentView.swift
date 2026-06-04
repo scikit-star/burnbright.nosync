286f942fba0dbee0f3e41f15711d1235f66f43b4
scikitstar <aksharaaramesh.computing@gmail.com>
2026-06-04 16:28:58 +0800

    deleted

--- burnbright/ContentView.swift
+++ burnbright/ContentView.swift
@@ -7,17 +7,70 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+enum Quote: CaseIterable{
+    case toxic, girlpower, belief, impulse, resilience
 }
+
+struct ContentView: View {
+    var myquote = Quote.resilience /*Quote.allCases.randomElement()!*/
+    @State var myowntime = 61200
+    var body: some View {
+        TabView {
+            Tab("Add new", systemImage: "flame.fill"){
+                switch myquote {
+                case .toxic:
+                    
+                    HStack {
+                        Spacer()
+                        VStack {
+                            Text("Tu veux être sur le podium,")
+                            Text("Tu n'as pas le droit aux mauvais jours")
+                        }
+                        Spacer()
+                    }
+                    .foregroundStyle(.red)
+                case .girlpower:
+                    HStack {
+                        Spacer()
+                        Text("Il n'est jamais mieux que moi")
+                        Spacer()
+                    }
+                    .foregroundStyle(.red)
+                case .belief:
+                    HStack {
+                        Spacer()
+                        Text("La douleur fait de moi une croyante")
+                        Spacer()
+                    }
+                    .foregroundStyle(.red)
+                case .impulse:
+                    HStack {
+                        Spacer()
+                        Text("M'investir et tenter ma chance")
+                        Spacer()
+                    }
+                    .foregroundStyle(.red)
+                case .resilience:
+                    HStack {
+                        Spacer()
+                        VStack {
+                            Text("Le monde brise tout le monde,et ensuite, beaucoup sont plus forts où ils ont été brisés")
+                        }
+                        
+                        Spacer()
+                    }
+                    .foregroundStyle(LinearGradient(
+                        colors: [.blue, .pink], startPoint: .topTrailing, endPoint: .bottomTrailing))
+                }
+                }
+            Tab("SJPO",systemImage: "soccerball"){
+                TimerView(mytime: $myowntime)
+            }
+                
+                        
+            }
+        }
+    }
 
 #Preview {
     ContentView()
