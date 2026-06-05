e6e7131ec34f102c292c17b0ca15b62e25943b6e
scikitstar <aksharaaramesh.computing@gmail.com>
2026-06-05 23:08:19 +0800

    added cards and draggability

--- burnbright/ContentView.swift
+++ burnbright/ContentView.swift
@@ -1,1 +1,29 @@
+import SwiftUI
 
+struct ContentView: View {
+    
+    @State var number: String = "2"
+    @State var changeView = false
+    var body: some View {
+        
+        if changeView{
+            draggabletestView()
+        }else{
+            Text("\(number)")
+                .frame(width: 150, height: 200)
+                .background(.red)
+                .cornerRadius(15)
+                .font(.system(size:40))
+            Button("New Screen"){
+                changeView = true
+            }
+        }
+        
+            
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
--- burnbright/draggabletestView.swift
+++ burnbright/draggabletestView.swift
@@ -8,37 +8,54 @@
 import SwiftUI
 
 struct draggabletestView: View {
-    @State var topDeck: [String] = ["1", "2", "3", "4", "5", "6"]
-    @State var mainPile: [String] = []
-    @State var bottomDeck: [String] = []
+    @State var topDeck: Set<String> = []
+    @State var mainPile: Set<String> = ["2"]
+    @State var bottomDeck: Set<String> = ["1", "2", "3"]
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            KanbanView(cards: topDeck, kandim:[390.0, 150.0], carddim: [20, 40], textsize: 30)
+                
+            Spacer()
+            KanbanView(cards: mainPile, kandim:[300.0, 300.0], carddim: [130, 200], textsize: 30)
+            Spacer()
+            KanbanView(cards: bottomDeck, kandim:[390.0, 150.0], carddim: [20, 40], textsize: 30)
+                .dropDestination(for: String.self) { droppedCards, location in
+                    
+                    mainPile.formUnion(Set(droppedCards))
+                    
+                    return true
+                } //sth wrong here but idk what, "Adding 'UIView' as a subview of UIHostingController.view is not supported and may result in a broken view hierarchy. Add your view above UIHostingController.view in a common superview or insert it into your SwiftUI content in a UIViewRepresentable instead."
+            
+        }
     }
 }
 struct KanbanView: View {
-    let title: String
-    let tasks: [String]
-    
+    let cards: Set<String>
+    let kandim: [Double]
+    let carddim: [Double]
+    let textsize: Double
     var body: some View {
         VStack (alignment: .leading){
-            Text(title).font(.footnote.bold())
             
             ZStack {
                 RoundedRectangle(cornerRadius: 15)
-                    .frame(maxWidth: .infinity)
+                    .frame(width: kandim[0], height: kandim[1])
                     .foregroundColor(Color(.secondarySystemFill))
                 
-                VStack(alignment: .leading, spacing: 12) {
-                    ForEach(tasks, id: \.self){ task in
-                        Text(task)
+                HStack(alignment: .center, spacing: 12) {
+                    ForEach(cards.sorted(), id: \.self){ card in
+                        Text(card)
+                            .frame(width: carddim[0], height: carddim[1])
                             .padding(12)
                             .background(Color(uiColor: .secondarySystemGroupedBackground))
                             .cornerRadius(8)
                             .shadow(radius: 1, x: 1, y: 1)
+                            .font(.system(size: textsize))
+                            .draggable(card)
                         
                     }
-                    Spacer()
+//                    Spacer()
                 }
                 .padding(.vertical)
             }
